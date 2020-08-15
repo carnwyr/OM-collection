46f3e75c445b34be5f77d014caf82ff6a2f50aa8
carnwyr <carnwyr@gmail.com>
2020-08-16 00:24:53 +0300

    Add search by card name

--- public/javascripts/cardsListFunctions.js
+++ public/javascripts/cardsListFunctions.js
@@ -1,7 +1,17 @@
+var typingTimer;
+var doneTypingInterval = 500;
+
 $(document).ready(function(){
 	resetFilters();
 	$("form :input").on('click', formChanged);
 	$("div#filters :input").on('change', filterApplied);
+	$("input#nameFilter").on('input', inputChanged);
+	/*$("input#nameFilter").('keydown', function(e) {
+		alert('a')
+		if (event.which == '13') {
+			event.preventDefault();
+		}
+	});*/
 });
 
 function formChanged(e) {
@@ -36,9 +46,10 @@ function filterApplied(e) {
 			if (index != 0) {
 				filters[formId] += ", ";
 			}
-	        filters[formId] += "." + $(obj).attr("name");
-	    });
+			filters[formId] += "." + $(obj).attr("name");
+		});
 	});
+	var search = $('input#nameFilter').val();
 
 	$(".cardPreview").fadeOut(400).promise().done(function() {
 		var cards = $(".cardPreview");
@@ -46,6 +57,12 @@ function filterApplied(e) {
 			if (filters[key] != "")
 				cards = $(cards).filter(filters[key]);
 		});
+		if (search != "") {
+			cards =$(cards).filter(function() {
+				var cardName = $(this).find("figcaption").text();
+				return cardName.toLowerCase().includes(search.toLowerCase());
+			});
+		}
 		cards.fadeIn(400);
 	});
 }
@@ -53,4 +70,10 @@ function filterApplied(e) {
 function resetFilters() {
 	$("input[type=checkbox]").prop('checked', false);
 	$("input[type=radio]").prop('checked', true);
+	$("input[type=text]").val("");
+}
+
+function inputChanged(e) {
+	clearTimeout(typingTimer);
+	typingTimer = setTimeout(filterApplied, doneTypingInterval);
 }
