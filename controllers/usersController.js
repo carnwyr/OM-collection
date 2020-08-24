a13f9389d1236e36a1ab87db11507e134e04d4ad
carnwyr <carnwyr@gmail.com>
2020-08-24 20:32:36 +0300

    Remove unnecessary checks in userController, make username case insensitive, fix confirmation input field

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -30,32 +30,20 @@ exports.signupGet = function(req, res, next) {
 };
 
 exports.signupPost = [
-	body('username')
-		.notEmpty().withMessage("Name can't be empty"),
+	//body('username')
 		//.isAscii().withMessage("Name contains invalid characters"),
-	body('password').trim()
-		.notEmpty().withMessage("Password can't be empty")
-		//.isAscii().withMessage("Password contains invalid characters")
-		.isLength({ min: 8 }).withMessage("Password must be at least 8 characters long"),
-	body('confirmPassword').trim()
-		.custom((value,{req, loc, path}) => {
-            if (value !== req.body.password) {
-                // trow error if passwords do not match
-                throw new Error("Passwords don't match");
-            } else {
-                return value;
-            }
-        }),
+	//body('password')
+		//.isAscii().withMessage("Password contains invalid characters"),
 	body('username').escape(),
 	body('password').escape(),
 	(req, res, next) => {
-		const errors = validationResult(req);
+		/*const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			res.render('signup', { title: 'Signup', user: req.user, errors: errors.array()});
 			return;
-		}
+		}*/
 
-		Users.findOne({ 'name': req.body.username }, function(err, isUserExist) {
+		Users.findOne({ 'name': { $regex : new RegExp('^' + req.body.username + '$', "i") } }, function(err, isUserExist) {
 			if (err) {return next(err); }
 
 			if (isUserExist) {
@@ -86,7 +74,7 @@ exports.signupPost = [
 ];
 
 exports.signupCheckUsername = function(req, res) {
-	Users.findOne({ 'name': req.body.username }, function(err, isUserExist) {
+	Users.findOne({ 'name': { $regex : new RegExp('^' + req.body.username + '$', "i") } }, function(err, isUserExist) {
 		if (err) { res.send('error'); return; }
 
 		if (isUserExist) {
@@ -110,7 +98,7 @@ exports.isLoggedIn = function () {
 
 passport.use(new LocalStrategy({ passReqToCallback : true },
 	function(req, username, password, next) {
-		Users.findOne({name: username}, function (err, user) {
+		Users.findOne({ 'name': { $regex : new RegExp('^' + username + '$', "i") }}, function (err, user) {
 			if (err) { return next(err) }
 			if (!user) {
 				return next(null, false, req.flash('message', 'No such user'))
