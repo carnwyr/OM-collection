4d501c3db71af23f571029f0b6fa19259a638215
carnwyr <carnwyr@gmail.com>
2020-09-03 19:04:57 +0300

    Change error page

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { user: req.user });
 });
 
 module.exports = app;
