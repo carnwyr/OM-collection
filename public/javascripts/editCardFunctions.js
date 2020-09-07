87d5eb4de6a13533f16528ccf4ccab5ce015686b
carnwyr <carnwyr@gmail.com>
2020-09-07 23:07:51 +0300

    Add auto-fill for unique name

--- public/javascripts/editCardFunctions.js
+++ public/javascripts/editCardFunctions.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
+    $('#name').on('focusout', fillUniqueName);
+
 	$("#uploadL").on('change', {extra: '#imageResultL'}, imageUploaded);
 	$("#uploadLB").on('change', {extra: '#imageResultLB'}, imageUploaded);
 	$("#uploadS").on('change', {extra: '#imageResultS'}, imageUploaded);
 
     $('.image-area button').on('click', removeImage);
 });
+
+function fillUniqueName() {
+    var name = $('#name').val();
+    var uniqueName = name.replace(/[\/:*?"<>|]+/g, '');
+    var uniqueName = uniqueName.replace(/ /g, '_');
+    $('#uniqueName').val(uniqueName);
+}
 
 function imageUploaded(e) {
 	if (this.files && this.files[0]) {
