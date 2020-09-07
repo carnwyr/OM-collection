464f26f0fbd41e9077fcf5fd91bcb0cd354b0b92
carnwyr <carnwyr@gmail.com>
2020-09-07 21:55:10 +0300

    Add remove button to images

--- public/javascripts/editCardFunctions.js
+++ public/javascripts/editCardFunctions.js
@@ -2,17 +2,25 @@ $(document).ready(function(){
 	$("#uploadL").on('change', {extra: '#imageResultL'}, imageUploaded);
 	$("#uploadLB").on('change', {extra: '#imageResultLB'}, imageUploaded);
 	$("#uploadS").on('change', {extra: '#imageResultS'}, imageUploaded);
+
+    $('.image-area button').on('click', removeImage);
 });
 
 function imageUploaded(e) {
-	console.log()
 	if (this.files && this.files[0]) {
         var reader = new FileReader();
 
         var imageArea = e.data.extra;
         reader.onload = function (e) {
             $(imageArea).attr('src', e.target.result);
+            $(imageArea).siblings('button').show();
         };
         reader.readAsDataURL(this.files[0]);
     }
+}
+
+function removeImage(e) {
+    e.preventDefault();
+    $(this).siblings('img').attr('src', '');
+    $(this).hide();
 }
