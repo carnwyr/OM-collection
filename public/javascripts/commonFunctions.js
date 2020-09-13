14cc391bf57f779fb0c952500ec57d7ddfcb841a
planet567 <thebirghteststar@gmail.com>
2020-09-13 17:29:18 -0400

    corrects typo in cookie

--- public/javascripts/commonFunctions.js
+++ public/javascripts/commonFunctions.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	$(".navbar .nav-item.active").removeClass('active');
 	$('.navbar .nav-item a[href="' + location.pathname + '"]').closest('li').addClass('active');
 
-	if (!document.cookie.split('; ').find(row => row.startsWith("hclosedSupportToast"))) {
+	if (!document.cookie.split('; ').find(row => row.startsWith("closedSupportToast"))) {
     $(".toast").toast("show");
   }
-	$("button#closeToast").on("click", () => { document.cookie = "hclosedSupportToast=true; expires=Fri, 31 Dec 2020 23:59:59 GMT"; })
+	$("button#closeToast").on("click", () => { document.cookie = "closedSupportToast=true; expires=Fri, 31 Dec 2020 23:59:59 GMT"; })
 });
