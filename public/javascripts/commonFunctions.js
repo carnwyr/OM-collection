58c2049c3862bfd080733940bcc9b0f812a0bd9e
planet567 <thebirghteststar@gmail.com>
2020-09-13 14:31:54 -0400

    added notification "please support"; updated discord invite link

--- public/javascripts/commonFunctions.js
+++ public/javascripts/commonFunctions.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
 	$(".navbar .nav-item.active").removeClass('active');
 	$('.navbar .nav-item a[href="' + location.pathname + '"]').closest('li').addClass('active');
+
+	$('.toast').toast('show');
 });
