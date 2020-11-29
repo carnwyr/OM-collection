9c36b94737e8e513c5107afd2b7ca44ad39bf5cf
starstarstar <67977772+the0star@users.noreply.github.com>
2020-11-28 19:39:02 -0500

    replaced placeholder text with correct text

--- public/javascripts/commonFunctions.js
+++ public/javascripts/commonFunctions.js
@@ -37,7 +37,7 @@ $(document).on("adBlocked", () => { adBlock = true; });
 
 $(window).on("load", () => {
 	if (window.location.pathname !== "/" && adBlock === true) {
-		$(".ad-container").append(`<div class="card card-body"><p class="col-md-8 mx-auto my-5">Karasu-OS.com is supported by donation and #########. <br>Please consider whitelisting karasu-os.com or making a donation to keep karasu alive for your enjoyment. <br><br><a class="btn supportBtn" href="https://www.buymeacoffee.com/karasuos" role="button" target="_blank">&#x1F608 Donate now</a></p></div>`);
+		$(".ad-container").append(`<div class="card card-body"><p class="col-md-8 mx-auto my-5">Karasu-OS.com is supported by donation and advertisement. <br>Please consider whitelisting karasu-os.com or making a donation to keep karasu alive for your enjoyment. <br><br><a class="btn supportBtn" href="https://www.buymeacoffee.com/karasuos" role="button" target="_blank">&#x1F608 Donate now</a></p></div>`);
 	};
 })
 
