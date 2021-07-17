cdc0a642a70eb6027db8158cbc3a0ff7b8e0dd4f
starstarstar <67977772+the0star@users.noreply.github.com>
2021-07-17 11:58:18 -0400

    add source fields

--- models/hiddenCards.js
+++ models/hiddenCards.js
@@ -6,6 +6,8 @@ var HiddenCardsSchema = new Schema({
 	name: { type: String, required: true },
 	uniqueName: { type: String, required: true, unique: true },
 	ja_name: { type: String, required: true },
+	source: { type: Array, required: true },
+	ja_source: { type: Array, required: true },
 	type: { type: String, required: true, enum: ["Demon", "Memory"] },
 	rarity: { type: String, required: true, enum: ["N", "R", "SR", "SSR", "UR", "UR+"] },
 	attribute: { type: String, required: true, enum: ["Pride", "Greed", "Envy", "Wrath", "Lust", "Gluttony", "Sloth"] },
