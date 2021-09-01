34f07d563dcba2c7e65b7e094ae6c9a0ac9187dd
starstarstar <67977772+the0star@users.noreply.github.com>
2021-08-31 22:14:44 -0400

    update countdown
    + new calculator idea

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -8,11 +8,19 @@ $(function() {
 
 function startCountdown(d) {
 	var countDownDate = new Date(d).getTime();
+	var today = new Date();
+
 	var x = setInterval(function() {
 	  var now = new Date().getTime();
 		var distance = countDownDate - now;
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-		// TODO: If day < 1, do the normal calculation; else, countdown to JSP 00:00
+		if (days > 0) {
+			var timezoneOffset = (today.getTimezoneOffset() + 9 * 60) * 60 * 1000;
+			var jst = new Date(now + timezoneOffset);  // get current time in jst
+
+			distance = new Date(2025, 0) - jst;
+		}
 
 	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
