02373ea4133ab0ef9c09d244760914d58a5a3d36
starstarstar <67977772+the0star@users.noreply.github.com>
2021-09-10 21:42:52 -0400

    update eslint

--- models/events.js
+++ models/events.js
@@ -3,30 +3,30 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var APSchema = new Schema({
-  amount: { type: Number, required: true },
-  points: { type: Number, required: true },
-  page: { type: Number }
+	amount: { type: Number, required: true },
+	points: { type: Number, required: true },
+	page: { type: Number }
 });
 
 var RewardsSchema = new Schema({
-  tag: { type: String, required: true },
-  points: { type: Number, required: true },
-  card: { type: String }
+	tag: { type: String, required: true },
+	points: { type: Number, required: true },
+	card: { type: String }
 });
 
 var EventsSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    ja_name: { type: String, required: true },
-    img_name: { type: String, required: true, unique: true },
-    type: { type: String, required: true, enum: ["Pop Quiz", "Lonely Devil", "Birthday", "Nightmare"] },
-    start: { type: Date },
-    end: { type: Date },
-    ap: { type: [APSchema], required: function() { return this.type !== "Nightmare"; } },
-    rewards: { type: [RewardsSchema], required: function() { return this.type !== "Nightmare"; } },
-    stages: { type: Number, required: function() { return this.type !== "Nightmare"; } },
-    pageCost: { type: Number, required: function() { return this.type !== "Nightmare"; } }
-  }
+	{
+		name: { type: String, required: true },
+		ja_name: { type: String, required: true },
+		img_name: { type: String, required: true, unique: true },
+		type: { type: String, required: true, enum: ["Pop Quiz", "Lonely Devil", "Birthday", "Nightmare"] },
+		start: { type: Date },
+		end: { type: Date },
+		ap: { type: [APSchema], required: function() { return this.type !== "Nightmare"; } },
+		rewards: { type: [RewardsSchema], required: function() { return this.type !== "Nightmare"; } },
+		stages: { type: Number, required: function() { return this.type !== "Nightmare"; } },
+		pageCost: { type: Number, required: function() { return this.type !== "Nightmare"; } }
+	}
 );
 
 
