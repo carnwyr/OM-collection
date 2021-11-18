a801f8e2711260279fed5877daaeb902ccba31e0
starstarstar <67977772+the0star@users.noreply.github.com>
2021-11-18 13:53:15 -0500

    optimize performance - fix translation - changed from cookie to localStorage - fix first contentful paint issue

--- public/javascripts/surpriseGuestFunctions.js
+++ public/javascripts/surpriseGuestFunctions.js
@@ -1,40 +1,23 @@
 $(document).ready(function() {
-	multipleOptions = document.cookie.split('; ').find(row => row.startsWith("sgpMultipleOptions"));
-	if (multipleOptions) {
-		multipleOptions = multipleOptions.split("=")[1] === 'true';
-	} else {
-		$('#settings').collapse('show');
-		multipleOptions = true;
-	}
-	useItems = document.cookie.split('; ').find(row => row.startsWith("sgpUseItems"))
-	useItems = useItems ? useItems.split("=")[1] === 'true' : true;
-
-	if (multipleOptions) {
-		$("#allActions").prop('checked', true);
-	} else {
-		$("#shortActions").prop('checked', true);
-	}
-	if (useItems) {
-		$("#useItems").prop('checked', true);
-	}
 	const urlParams = new URLSearchParams(window.location.search);
-	const character = urlParams.get('character');
-	if (character) {
-		openCharacterTab(character);
-	}
+	var character = urlParams.get('character')?urlParams.get('character'):"Lucifer";
+
+	updateSettingsOnLoad();
+	openCharacterTab(character);
 
 	$("#shortActions").click(() => enableOptions(false));
 	$("#allActions").click(() => enableOptions(true));
 	$("#useItems").click(() => enableItemOptions($("#useItems").is(":checked")));
-	enableOptions($("#allActions").is(":checked"));
 	$('.big-nav>.nav-link, .small-nav>.nav-link').click(function() {syncButtons($(this), urlParams)});
+	$("#settingsCollapser").click(() => {
+		localStorage.setItem("displaySettings", !$("#settings").hasClass("show"));
+	});
 });
 
 function enableOptions(enableAll) {
+	localStorage.setItem("multipleOptions", enableAll);
 	if (enableAll) {
-		document.cookie = "sgpMultipleOptions=true; expires=Tue, 19 Jan 2038 00:00:00 UTC; SameSite=Lax";
-		$(".movesShort").hide();
-		$(".itemsShort").hide();
+		$(".movesShort, .itemsShort").hide();
 		$(".moves").show();
 		if ($("#useItems").is(":checked")) {
 			$(".items").show();
@@ -42,9 +25,7 @@ function enableOptions(enableAll) {
 			$(".items").hide();
 		}
 	} else {
-		document.cookie = "sgpMultipleOptions=false; expires=Tue, 19 Jan 2038 00:00:00 UTC; SameSite=Lax";
-		$(".moves").hide();
-		$(".items").hide();
+		$(".moves, .items").hide();
 		$(".movesShort").show();
 		if ($("#useItems").is(":checked")) {
 			$(".itemsShort").show();
@@ -55,17 +36,15 @@ function enableOptions(enableAll) {
 }
 
 function enableItemOptions(allowItems) {
+	localStorage.setItem("useItems", allowItems);
 	if (allowItems) {
-		document.cookie = "sgpUseItems=true; expires=Tue, 19 Jan 2038 00:00:00 UTC; SameSite=Lax";
 		if ($("#shortActions").is(":checked")) {
 			$(".itemsShort").show();
 		} else {
 			$(".items").show();
 		}
 	} else {
-		document.cookie = "sgpUseItems=false; expires=Tue, 19 Jan 2038 00:00:00 UTC; SameSite=Lax";
-		$(".items").hide();
-		$(".itemsShort").hide();
+		$(".items, .itemsShort").hide();
 	}
 }
 
@@ -80,7 +59,42 @@ function syncButtons(button, urlParams) {
 	window.history.replaceState(null, null, `${window.location.pathname}${urlParams.toString()===''?'':'?'+urlParams.toString()}`);
 }
 
+function updateSettingsOnLoad() {
+	var multipleOptions = localStorage.getItem("multipleOptions");
+	var useItems = localStorage.getItem("useItems");
+	var displaySettings = localStorage.getItem("displaySettings");
+
+	// convert values
+	if (multipleOptions === null) {
+		multipleOptions = true;
+	} else {
+		multipleOptions = (multipleOptions === "true");
+	}
+	if (useItems === null) {
+		useItems = true;
+	} else {
+		useItems = (useItems === "true");
+	}
+	if (displaySettings === null) {
+		displaySettings = true;
+		localStorage.setItem("displaySettings", false);
+	} else {
+		displaySettings = (displaySettings === "true");
+	}
+
+	if (multipleOptions) {
+		$("#allActions").prop('checked', true);
+	} else {
+		$("#shortActions").prop('checked', true);
+	}
+	$("#useItems").prop('checked', useItems);
+	enableOptions(multipleOptions);
+
+	if (displaySettings) {
+		$("#settings").collapse("show");
+	}
+}
+
 function openCharacterTab(character) {
-	$('#'+character+'Pill').tab('show');
-	$('#'+character+'PillSmall').tab('show');
+	$('#'+character+'Pill, #'+character+'PillSmall').tab('show');
 }
