998d7da1bf883497aa6753461ec27ca96f5c8155
starstarstar <67977772+the0star@users.noreply.github.com>
2022-02-13 12:10:56 -0500

    disable ad-block message

--- public/javascripts/commonFunctions.js
+++ public/javascripts/commonFunctions.js
@@ -36,12 +36,12 @@ $(document).ready(function() {
 
 	$("#b2t").on("click", () => { $("html, body").animate({scrollTop:0}, 500); });
 
-	setTimeout(() => {
-		if ($(".ezoic-ad.adtester-container div").height() === 0) {
-			$("#kool-aid>.row").hide();
-			$("#kool-aid").append(`<div class="card shadow-none" style="background:rgba(255,255,255,.8)"><div class="card-body col-md-8 mx-auto" style="border-radius:1rem;"><h5 class="card-title">Using an ad-blocker?</h5><img style="width:7rem;height:auto;margin-bottom:1rem;" src="/images/adblocked.png"><p>Advertisements help us cover the cost to keep karasu-os online.</p><p>Please consider whitelisting karasu-os.com to keep the website free for everyone!</p></div></div>`);
-		}
-	}, 1000);
+	// setTimeout(() => {
+	// 	if ($(".ezoic-ad.adtester-container div").height() === 0) {
+	// 		$("#kool-aid>.row").hide();
+	// 		$("#kool-aid").append(`<div class="card shadow-none" style="background:rgba(255,255,255,.8)"><div class="card-body col-md-8 mx-auto" style="border-radius:1rem;"><h5 class="card-title">Using an ad-blocker?</h5><img style="width:7rem;height:auto;margin-bottom:1rem;" src="/images/adblocked.png"><p>Advertisements help us cover the cost to keep karasu-os online.</p><p>Please consider whitelisting karasu-os.com to keep the website free for everyone!</p></div></div>`);
+	// 	}
+	// }, 1000);
 });
 
 $(window).on("scroll", () => {
