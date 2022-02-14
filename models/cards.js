0beb1db533a12f0d3ad786a15508190fe8348eaf
starstarstar <67977772+the0star@users.noreply.github.com>
2022-02-14 13:21:45 -0500

    remove ja_source

--- models/cards.js
+++ models/cards.js
@@ -5,7 +5,6 @@ const cardSchema = new mongoose.Schema({
 	uniqueName: { type: String, required: true, unique: true },
 	ja_name: { type: String, required: true },
 	source: { type: Array, required: true },
-	ja_source: { type: Array, required: true },  // TODO: remove
 	type: { type: String, required: true, enum: ["Demon", "Memory"] },
 	rarity: { type: String, required: true, enum: ["N", "R", "SR", "SSR", "UR", "UR+"] },
 	attribute: { type: String, required: true, enum: ["Pride", "Greed", "Envy", "Wrath", "Lust", "Gluttony", "Sloth"] },
