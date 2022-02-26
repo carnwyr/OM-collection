0bb26d1fc0810503adab0dbb6eaad2e8b2795118
carnwyr <carnwyr@gmail.com>
2022-02-26 13:16:29 +0300

    Revert "Prevent user creation attempt when username is taken"
    This reverts commit 7634b564c07c7d8f61063b3c47b90f9eda291795.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -77,7 +77,7 @@ exports.signup = [
 
     if (exists) {
       req.flash("message", "Username taken");
-      return res.render("signup", { title: "Signup", user: req.user });
+      res.render("signup", { title: "Signup", user: req.user });
     } else {
       bcrypt.genSalt(Number.parseInt(process.env.SALT_ROUNDS), (err, salt) => {
         if (err) return next(err);
