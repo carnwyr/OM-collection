ccada2b93160c35ee11449280f463b232d31360c
starstarstar <67977772+the0star@users.noreply.github.com>
2022-03-02 14:32:10 -0500

    bring back getRowCapacity

--- public/javascripts/cardsListFunctions.js
+++ public/javascripts/cardsListFunctions.js
@@ -374,3 +374,19 @@ function addCardsToChangedList(cardsToSwitch, select) {
 	});
 	cardsToAdd.forEach(uniqueName => changedCards[uniqueName] = select);
 }
+
+function getRowCapacity() {
+	// max number of cards for xs is 4, on smaller screens it's reduced by 1 (possibly outdated)
+	let cardsInRow;
+	if (viewType === "icon") {
+		cardsInRow = { 576: Math.floor(($(window).width() - 100) / 100), 768: 6, 992: 6, 1200: 7, xl: 9 };
+	} else {
+		cardsInRow = { 576: Math.floor(($(window).width() - 100) / 100), 768: 4, 992: 3, 1200: 4, xl: 5 };
+	}
+
+	for (let [size, cardCount] of Object.entries(cardsInRow)) {
+		if (size === "xl" || $(window).width() <= screen) {
+			return cardCount;
+		}
+	}
+}
