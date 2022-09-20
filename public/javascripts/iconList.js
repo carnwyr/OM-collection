f5e3dfc120bfb702000dd5f36092acb5a8e81ae5
starstarstar <67977772+the0star@users.noreply.github.com>
2022-09-20 14:54:14 -0400

    Update iconList.js

--- public/javascripts/iconList.js
+++ public/javascripts/iconList.js
@@ -1,7 +1,7 @@
 $(function() {
   cards.filter(x => x.type === "Demon").forEach((card, i) => {
     let cardName = encodeURIComponent(card.name.replace(/_/g, "_").replace(/:/g, ","));
-    let text = `<div class="col-3 py-2 px-3 text-center"><a href="/card/${cardName}">
+    let text = `<div class="col-6 col-md-4 col-lg-3 py-2 px-3 text-center"><a href="/card/${cardName}">
     <img class="mr-2" src="https://obey-me.fandom.com/wiki/Special:Redirect/file/${cardName}_icon.png?width=128" onerror="this.src='/images/reward_placeholder.png'" style="width:46%;height:auto;">
     <img src="https://obey-me.fandom.com/wiki/Special:Redirect/file/${cardName}_Unlocked_icon.png?width=128" onerror="this.src='/images/reward_placeholder.png'" style="width:46%;height:auto;">
     </a></div>`;
@@ -11,8 +11,8 @@ $(function() {
     if (card.rarity !== "SR") {
       let cardName = encodeURIComponent(card.name.replace(/_/g, "_").replace(/:/g, ","));
       let characterNum = card.characters.indexOf(character) + 1;
-      let text = `<div class="col p-2 text-center"><a href="/card/${cardName}">
-      <img src="https://obey-me.fandom.com/wiki/Special:Redirect/file/${cardName}_${characterNum}_icon.png?width=128" onerror="this.src='/images/reward_placeholder.png'" style="width:128px;height:auto;">
+      let text = `<div class="col-3 col-md-2 p-2 text-center"><a href="/card/${cardName}">
+      <img src="https://obey-me.fandom.com/wiki/Special:Redirect/file/${cardName}_${characterNum}_icon.png?width=128" onerror="this.src='/images/reward_placeholder.png'" style="width:100%;max-width:128px;height:auto;">
       </a></div>`;
       $("#memory").append(text);
     }
