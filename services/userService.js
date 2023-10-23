dd112ec825ffccbc5fe4685846a327ad0b32f221
starstarstar <67977772+the0star@users.noreply.github.com>
2023-10-22 22:18:25 -0400

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -28,7 +28,8 @@ exports.getAccountData = async function (username) {
       }
     }
   ]);
-  if (!user[0].profile?.display) {
+  if (!user[0].profile) user[0].profile = {};
+  if (!user[0].profile.display) {
     user[0].profile.display = "The_Mammon_Way";
   }
   return user[0];
